9d9cd3cebd280310983bc9a15a1e2b55a0da5cd5
huynhtanhoa3011 <85146749+huynhtanhoa3011@users.noreply.github.com>
2021-07-27 20:24:25 +0700

    Add files via upload

---
+++ hienthithoigian.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class hienthithoigian {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now: " + now);
+    }
+}

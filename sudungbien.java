75a0650187fc5127b0cb3b487d2b524a465845cb
huynhtanhoa3011 <85146749+huynhtanhoa3011@users.noreply.github.com>
2021-07-27 21:15:25 +0700

    Add files via upload

---
+++ sudungbien.java
@@ -0,0 +1,17 @@
+public class sudungbien {
+    public static void main(String[] args) {
+        int i = 10; // i=10 là số nguyên nên dùng kdl là int
+        float f = 20.5f; // f = 20.5f là số thực nên dùng kdl là float
+        double d = 20.5; // d = 20.5 là số thực có kích thước 8byte nên dùng kdl là double
+        boolean b = true; // b = true dùng kdl boolean
+        char c = 'a'; // c = 'a' là ký tự nên dùng kdl là char
+        String s = "Hà Nội"; // s = "Hà Nội' là chuỗi nên dùng kdl là String
+
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}

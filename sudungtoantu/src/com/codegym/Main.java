3d56ad5746ad81458281f4ad745b76bf61a81969
huynhtanhoa3011 <85146749+huynhtanhoa3011@users.noreply.github.com>
2021-07-27 14:13:45 +0700

    Add files via upload

---
+++ sudungtoantu/src/com/codegym/Main.java
@@ -0,0 +1,22 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	float width;
+	float height;
+
+        Scanner scanner = new Scanner(System.in); //	khai báo đối tượng Scanner
+        System.out.println("Enter width");
+        width = scanner.nextFloat();
+        System.out.println("Enter height");
+        height = scanner.nextFloat();
+
+        float area = width * height;
+
+        System.out.println("Area is: " + area);
+
+    }
+}

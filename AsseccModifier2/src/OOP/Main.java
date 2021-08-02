4d84e0c33c1c65e327842036d875b23806cf5b35
huynhtanhoa3011 <85146749+huynhtanhoa3011@users.noreply.github.com>
2021-08-02 09:47:13 +0700

    Add files via upload

---
+++ AsseccModifier2/src/OOP/Main.java
@@ -0,0 +1,11 @@
+package OOP;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Student student = new Student();
+        System.out.println("Student name is: " + student.name);
+        System.out.println("Student classes is: " + student.classes);
+    }
+}
---
+++ AsseccModifier2/src/OOP/Student.java
@@ -0,0 +1,16 @@
+package OOP;
+
+public class Student {
+    public String name = "John";
+    public String classes = "C02";
+
+    public Student () {}
+
+    public String setName () {
+        return this.name;
+    }
+    public String setClasses () {
+        return this.classes;
+    }
+
+}

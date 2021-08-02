000683fd0a6cd5c7d74620ebce0a31b6a4e16742
huynhtanhoa3011 <85146749+huynhtanhoa3011@users.noreply.github.com>
2021-08-02 08:32:27 +0700

    Add files via upload

---
+++ StaticMethod/src/OOP/Main.java
@@ -0,0 +1,16 @@
+package OOP;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Student.change();
+        Student s1 = new Student(111, "Hoang");
+        Student s2 = new Student(222, "Khanh");
+        Student s3 = new Student(333, "Nam");
+
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+}

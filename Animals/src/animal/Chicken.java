8c372131c70c56245e02b53bbd103577ca552485
huynhtanhoa3011 <85146749+huynhtanhoa3011@users.noreply.github.com>
2021-08-04 11:04:53 +0700

    Add files via upload

---
+++ Animals/src/animal/Chicken.java
@@ -0,0 +1,8 @@
+package animal;
+
+public class Chicken extends Animal{
+    @Override
+    public String makeSuond() {
+        return "Chicken: cluck-cluck";
+    }
+}
---
+++ Animals/src/animal/Tiger.java
@@ -0,0 +1,8 @@
+package animal;
+
+public class Tiger extends Animal{
+    @Override
+    public String makeSuond() {
+        return "Tiger: groarrrr";
+    }
+}

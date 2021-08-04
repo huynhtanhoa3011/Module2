da7044a4608a6e5f3b59808930f5cd60506aad81
huynhtanhoa3011 <85146749+huynhtanhoa3011@users.noreply.github.com>
2021-08-04 14:27:18 +0700

    Add files via upload

---
+++ Colorable/src/Colorable/Main.java
@@ -0,0 +1,18 @@
+package Colorable;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Square s1 = new Square(true, 5);
+        System.out.println(s1);
+        s1.howToColor();
+
+        s1 = new Square(10);
+        System.out.println(s1);
+        s1.howToColor();
+
+        s1 = new Square();
+        System.out.println(s1);
+        s1.howToColor();
+    }
+}

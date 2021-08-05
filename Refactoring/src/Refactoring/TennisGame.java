e0aadac7d83ac6f42a2c8fffa708611cd2014eb3
huynhtanhoa3011 <85146749+huynhtanhoa3011@users.noreply.github.com>
2021-08-05 13:50:49 +0700

    Add files via upload

---
+++ Refactoring/src/Refactoring/TennisGame.java
@@ -0,0 +1,63 @@
+package Refactoring;
+
+public class TennisGame {
+    public static String getScore(String player1Name, String player2Name, int point1, int point2) {
+        String score = " ";
+        int temScore = 0;
+        boolean sameScore = point1 == point2;
+        if (sameScore) {
+            switch (point1) {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fitteen-All";
+                    break;
+                case 2:
+                    score = "thirty-All";
+                    break;
+                case 3:
+                    score = "Forty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
+            }
+        } else {
+            boolean result = point1 >= 4 || point2 >= 4;
+            if (result) {
+
+                int minusResult = point1 - point2;
+                if (minusResult == 1) score = "Advantage player1";
+                else if (minusResult == -1) score = "Advantage player2";
+                else if (minusResult >= 2) score = "Win for player1";
+                else score = "Win for player2";
+            } else {
+                for (int i = 1; i < 3; i++) {
+                    int tempScore;
+                    if (i == 1) tempScore = point1;
+                    else {
+                        score += "-";
+                        tempScore = point2;
+                    }
+                    switch (tempScore) {
+                        case 0:
+                            score += "Love";
+                            break;
+                        case 1:
+                            score += "Fifteen";
+                            break;
+                        case 2:
+                            score += "Thirty";
+                            break;
+                        case 3:
+                            score += "Forty";
+                            break;
+                    }
+                }
+            }
+        }
+        return score;
+    }
+}
+

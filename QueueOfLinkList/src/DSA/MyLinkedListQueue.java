dbff5751562e47dd9f0a42df970df475772057df
huynhtanhoa3011 <85146749+huynhtanhoa3011@users.noreply.github.com>
2021-08-11 08:33:21 +0700

    Add files via upload

---
+++ QueueOfLinkList/src/DSA/MyLinkedListQueue.java
@@ -0,0 +1,33 @@
+package DSA;
+
+public class MyLinkedListQueue {
+    private Node head;
+    private Node tail;
+
+    public MyLinkedListQueue() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    // setting method in class MyLinkedListQueue
+    public void enqueue(int key) {
+        Node temp = new Node(key);
+        if (this.tail == null) {
+            this.head = this.tail = temp;
+            return;
+        }
+        this.tail.next = temp;
+        this.tail = temp;
+    }
+
+    // setting method in class MyLinkedListQueue
+    public Node dequeue() {
+        if (this.head == null)
+            return null;
+        Node temp = this.head;
+        this.head = this.head.next;
+        if (this.head == null)
+            this.tail = null;
+        return temp;
+    }
+}

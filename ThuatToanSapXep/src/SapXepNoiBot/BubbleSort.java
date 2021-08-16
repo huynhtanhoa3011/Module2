863069bf878c8850315782cd5debb4a491274288
huynhtanhoa3011 <85146749+huynhtanhoa3011@users.noreply.github.com>
2021-08-16 09:11:00 +0700

    ThuatToanSapXep

--- ThuatToanSapXep/src/SapXepNoiBot/BubbleSort.java
+++ ThuatToanSapXep/src/SapXepNoiBot/BubbleSort.java
@@ -3,11 +3,12 @@ package SapXepNoiBot;
 public class BubbleSort {
 //    khai báo mảng số nguyên chưa sắp xếp
     static int[] list = {2, 3, 2, 5, 6, 1, -2, 14, 12};
-
+//      cài đặt phương thức bubbleSort(int[]list)
     public static void bubbleSort(int[] list) {
         boolean needNextPass = true;
 
         for (int k = 1; k < list.length && needNextPass; k++) {
+//            mảng có thể được sắp xếp mà không cần chuyển tiếp
             needNextPass = false;
             for (int i = 0; i < list.length -k; i++) {
                 if (list[i] > list[i + 1]) {
@@ -20,7 +21,7 @@ public class BubbleSort {
             }
         }
     }
-
+// cài đặt phương thức main để thực thi ứng dụng
     public static void main(String[] args) {
         bubbleSort(list);
         for (int i = 0; i < list.length; i++)
